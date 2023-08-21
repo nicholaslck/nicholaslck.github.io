6507988ecc688a0d81855fcd74df8e5cd7d83831
Nicholas Lau <nicholaslck@gmail.com>
2023-08-21 23:22:09 +0800

    init svelte project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<div class="max-w-[1200px] m-auto mt-8">
+	<h1 class='text-xl'>Nicholas's Personal Site</h1>
+	<p>Welcome, visitor!</p>
+	<p>
+		Well... as you can see, there are nothing here. Previously I have my own personal site, but I
+		decided not to continue that domain and wanted to move to github.io. At the same time, I think,
+		why not do a revamp of the site and make it more a comfortable space for me to publish contents?
+		So here it is, my new site, in progress. I will try to post some of my thoughts here, and maybe
+		some of my projects. I hope you enjoy your stay here. Stay tuned.
+	</p>
+</div>
