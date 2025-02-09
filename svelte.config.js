4ffc22a7cd57885fd0243afcac998f530e9dc468
Nicholas Lau <nicholaslck@gmail.com>
2025-02-09 13:24:00 +0800

    build static

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,15 @@ const config = {
 	preprocess: [vitePreprocess(), mdsvex()],
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
 	},
 
 	extensions: ['.svelte', '.svx']
